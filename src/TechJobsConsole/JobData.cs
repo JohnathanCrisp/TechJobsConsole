7a19f00ebfffe7dcfb05c6beda5ec40b03cd5ab4
Johnathan <johnathan.m.crisp@gmail.com>
2020-07-07 12:44:21 -0500

    finished project

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -49,7 +49,7 @@ namespace TechJobsConsole
             {
                 string aValue = row[column];
 
-                if (aValue.Contains(value))
+                if (aValue.ToLower().Contains(value.ToLower()))
                 {
                     jobs.Add(row);
                 }
@@ -137,6 +137,26 @@ namespace TechJobsConsole
             valueBuilder.Clear();
 
             return rowValues.ToArray();
+        }
+        public static List<Dictionary<string,string>> FindByValue(string someValue)
+        {
+            LoadData();
+
+            List<Dictionary<string, string>> jValues = new List<Dictionary<string, string>>();
+
+            foreach (Dictionary<string, string>Teddy in AllJobs)
+            {
+                foreach (string testValue in Teddy.Values)
+                {
+                    if (testValue.ToLower().Contains(someValue.ToLower()))
+                    {
+                        jValues.Add(Teddy);
+                        break;
+                    }
+                }
+                
+            }
+            return jValues;
         }
     }
 }
--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -63,7 +63,10 @@ namespace TechJobsConsole
                     // Fetch results
                     if (columnChoice.Equals("all"))
                     {
-                        Console.WriteLine("Search all fields not yet implemented.");
+                        searchResults = JobData.FindByValue(searchTerm);
+                        PrintJobs(searchResults);
+
+                        /*Console.WriteLine("Search all fields not yet implemented.");*/
                     }
                     else
                     {
@@ -118,7 +121,29 @@ namespace TechJobsConsole
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
-            Console.WriteLine("PrintJobs is not implemented yet");
-        }
+
+            if (someJobs.Count == 0)
+            {
+                Console.WriteLine("Stop doing that.");
+            }
+            else
+            {
+                int counter = 1;
+                foreach (Dictionary<string, string> job in someJobs)
+                {
+                    Console.WriteLine("****************************************");
+
+                    foreach (KeyValuePair<string, string> name in job)
+                    {
+
+                        Console.WriteLine("* " + name.Key + " : " + name.Value);
+
+                    }
+                    Console.WriteLine("****************************************\n");
+
+                    Console.WriteLine(counter++);
+                }   
+            }    //Console.WriteLine("PrintJobs is not implemented yet");
+        }     
     }
 }
